d7d194fd166376ae87b1e4f6ca589a923ebc9c09
João Vaz <joao.guimaraes.alves.vaz@gmail.com>
2021-05-12 16:12:40 -0300

    Add 05/12 files

---
+++ lectures/05/12/js/script.js
@@ -0,0 +1,42 @@
+let num = 42
+let name = 'joao'
+let shopping_list = ['rice', 'beans', 'juice', 'bread']
+let bool = true
+
+let age = 18
+let gender = 'male'
+
+        true
+    true && true
+if(age >= 18 && gender == 'female') {
+    // something that will be executed only if age >= 18 and gender is 'female'
+}
+
+/*
+    Data types:
+        Number
+        Strings (Text)
+        Array (List)
+        Booleans (true/ false values)
+
+    George Boole - matematico
+
+
+    Truth table
+
+    false or true = true
+    true or false = true
+    true or true = true
+    false or false = false
+    false and true = false
+    true and false = false
+    true && true = true
+    false and false = false
+    not true = false
+    not false = true
+
+    if(raining_outside) get_an_umbrealla()
+    else do_not_get_an_umbrella()
+
+
+*/
